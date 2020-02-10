6901897b6098e793c6c8056a6e8cdc6b47ae28d0
ibaiC <ibaicastells@gmail.com>
2020-02-10 16:52:04 +0000

    Finished bytes to image method

--- App/src/app/App.java
+++ App/src/app/App.java
@@ -4,33 +4,34 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import javax.imageio.ImageIO;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(TextToBinary("hello dawg"));
+        // System.out.println(TextToBinary("hello dawg"));
         // TextToBinary("hello dawg");
-        System.out.println(BinaryToText(TextToBinary("hello dawg")));
-        System.out.println("Hello Java");
-        imageToBitMap("/home/ibai/Documents/Uni/forensics/workspace/forensics/App/res/boats.bmp");
+        // System.out.println(BinaryToText(TextToBinary("hello dawg")));
+        // System.out.println("Hello Java");
+        // imageToBitMap("/home/ibai/Documents/Uni/forensics/workspace/forensics/App/res/boats.bmp");
+        // ByteArrayToImage(imageToBitMap("/home/ibai/Documents/Uni/forensics/workspace/forensics/App/res/boats.bmp"));
+        // System.out.println(System.getProperty("user.dir"));
     }
 
     // public static Number[][] imageToBitMap(Image image){
-    //     return [1][0];
+    // return [1][0];
 
-    // } 
-    public static String TextToBinary(String text){
+    // }
+    public static String TextToBinary(String text) {
         byte[] bytes = text.getBytes();
         StringBuilder binary = new StringBuilder();
-        for (byte b : bytes)
-        {
-           int val = b;
-           for (int i = 0; i < 8; i++)
-           {
-              binary.append((val & 128) == 0 ? 0 : 1);
-              val <<= 1;
-           }
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
         }
         System.out.println("'" + text + "' to binary: " + binary);
         return binary.toString();
@@ -38,30 +39,34 @@ public class App {
 
     public static byte[] imageToBitMap(String ImagePath) throws IOException {
 
-
         try {
             BufferedImage originalImage = ImageIO.read(new File(ImagePath));
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write( originalImage, "bmp", baos );
+            ImageIO.write(originalImage, "bmp", baos);
             baos.flush();
-            byte [] imageInByte = baos.toByteArray();
+            byte[] imageInByte = baos.toByteArray();
             baos.close();
             System.out.println(Arrays.toString(imageInByte));
             System.out.println(imageInByte.length);
             return imageInByte;
-        }   
+        }
 
-        catch(IOException e){
+        catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    public static void ByteArrayToImage(byte[] bitMap) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bitMap);
+        BufferedImage newImage = ImageIO.read(bis);
+        ImageIO.write(newImage, "bmp", new File("/home/ibai/Documents/Uni/forensics/workspace/forensics/App/res/output.bmp"));
+        System.out.println("The new image has been saved successfully! DEBUG: Currently gets saved to /res folder as output.bmp");
+        // To get current directory do;
+        // System.getProperty("user.dir");
     } 
 
-    // public static Number[][] BitMapToImage(Number[][] bitMap){
-    //     return image;
-
-    // } 
     public static String BinaryToText(String binary){
         String Text = "";
         String bit;
